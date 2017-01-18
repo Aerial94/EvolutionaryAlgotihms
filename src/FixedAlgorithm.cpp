cbe5f0a49a4de3d6677197d286fd6aca1a801471
Aleksander Brodowicz <aleksanderbrodowicz@gmail.com>
2017-01-18 21:27:23 +0100

    feat: builder for FixedAlgorithm

--- src/FixedAlgorithm.cpp
+++ src/FixedAlgorithm.cpp
@@ -5,45 +5,63 @@
 #include <algorithm>
 #include <chrono>
 #include "FixedAlgorithm.h"
+#include <iostream>
 
 FixedAlgorithm::FixedAlgorithm(std::shared_ptr<Problem> problem, size_t duration, unsigned numberOfPopulation)
         : IEvolutionaryAlgorithm(problem), duration(duration), numberOfPopulation(numberOfPopulation), currentPopulation(0),
             currentIteration(0){
     population = problem->generate();
+    connectIntoPairs(population, 0);
 
 }
 
 std::vector<std::shared_ptr<IElement>> FixedAlgorithm::reproduce() {
     std::vector<std::shared_ptr<IElement>> population = std::move(getPopulation());
     std::vector<std::shared_ptr<IElement>> newPopulation;
-    for (std::shared_ptr<IElement> &element: population) {
+    unsigned seed = (unsigned int) std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<int> distribution(0, population.size() - 1);
+    for (int i = 0 ; i < 2; ++i) {
+        std::shared_ptr<IElement> element = population[distribution(generator)];
         std::vector<std::shared_ptr<IElement>> neighbours = problem->select(element);
         newPopulation.insert(newPopulation.end(), neighbours.begin(), neighbours.end());
     }
     connectIntoPairs(newPopulation, population.size());
-    population.insert(newPopulation.end(), newPopulation.begin(), newPopulation.end());
+    population.insert(population.end(), newPopulation.begin(), newPopulation.end());
     return population;
 }
 
 void FixedAlgorithm::success(std::vector<std::shared_ptr<IElement>> &tempPopulation) {
     currentPopulation += 1;
     if(currentIteration < duration){
+        ++currentIteration;
         population = tempPopulation;
         return;
     }
     std::sort(connectedPairs.begin(), connectedPairs.end(),
               [&](std::pair<int, int> &a,std::pair<int, int> &b) -> bool {
-                  return problem->rate(tempPopulation[a.first]) +  problem->rate(tempPopulation[a.second]) <
+                  return problem->rate(tempPopulation[a.first]) +  problem->rate(tempPopulation[a.second]) >
                           problem->rate(tempPopulation[b.first]) +  problem->rate(tempPopulation[b.second]);
               });
-    population = std::vector<std::shared_ptr<IElement>>(tempPopulation.begin(),
-                                                        tempPopulation.begin() +
-                                                        std::min(tempPopulation.size(), problem->getPopulationSize()));
+    std::vector<std::shared_ptr<IElement>> newPopulation;
+    for(std::pair<int,int> pair: connectedPairs) {
+        if(newPopulation.size() < problem->getPopulationSize()) {
+            newPopulation.push_back(tempPopulation[pair.first]);
+            newPopulation.push_back(tempPopulation[pair.second]);
+        } else {
+            break;
+        }
+    }
+    population = std::vector<std::shared_ptr<IElement>>(newPopulation);
     currentIteration = 0;
-    connectedPairs.clear();
+    connectedPairs = std::vector<std::pair<int, int>>();
 }
 
 std::vector<std::shared_ptr<IElement>> FixedAlgorithm::mutate(std::vector<std::shared_ptr<IElement>> &tempPopulation) {
+    std::vector<std::shared_ptr<IElement>> newPopulation;
+    for(std::shared_ptr<IElement> element: tempPopulation) {
+        newPopulation.push_back(element->mutate());
+    }
     return std::vector<std::shared_ptr<IElement>>(tempPopulation);
 }
 
@@ -55,17 +73,21 @@ FixedAlgorithm::crossover(std::vector<std::shared_ptr<IElement>> &tempPopulation
 void FixedAlgorithm::connectIntoPairs(std::vector<std::shared_ptr<IElement>> &tempPopulation, size_t populationSize) {
     std::vector<int> myVec;
     for(int i = populationSize; i < populationSize + tempPopulation.size(); i++ )
-        myVec.push_back( i );
+        myVec.push_back(i);
     unsigned seed = (unsigned int) std::chrono::system_clock::now().time_since_epoch().count();
+    std::cout<<myVec.size()<<std::endl;
 
     std::shuffle(myVec.begin(), myVec.end(), std::default_random_engine(seed));
-    for(int i = 0; i< myVec.size()-1; i = i + 2) {
+    std::cout<<myVec.size()<<std::endl;
+
+    for(int i = 0; i < myVec.size()-1; i = i + 2) {
         std::pair<int, int> pair(myVec[i], myVec[i+1]);
         connectedPairs.push_back(pair);
     }
 }
 
 bool FixedAlgorithm::finish() {
+
     return currentPopulation > numberOfPopulation;
 }
 
